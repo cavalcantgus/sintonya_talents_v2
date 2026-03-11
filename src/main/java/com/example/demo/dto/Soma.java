c55ae499a3c6a1e5a9f9937a17ba4a98edb1c76d
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2026-03-11 12:52:15 -0300

    Subindo volumes mapeados

---
+++ src/main/java/com/example/demo/dto/Soma.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+public class Soma {
+    public int soma(int a, int b) {
+//        System.out.println(a + b);
+
+        return a + b;
+    }
+}
+
+// int result = soma(1, 2)
